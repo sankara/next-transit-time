bf8297297400114fd9a4df5a6318b128263de131
Sankara Rameswaran <hello@sankara.me>
2018-10-14 18:43:02 -0700

    Formatting for alexa

--- src/clj/next_transit/routes/services.clj
+++ src/clj/next_transit/routes/services.clj
@@ -4,6 +4,14 @@
             [schema.core :as s]
             [next-transit.next-ferry :as ferry]))
 
+(defn- to-alexa-response [{depart :depart, arrive :arrive}]
+  (let [output-text (str "The next ferry is at " depart ", and will arrive at " arrive)]
+    {:version "1.0"
+     :response {:outputSpeech {:type "PlainText"
+                               :text output-text}}}))
+
+;;(to-alexa-response {:depart "22:00" :arrive "22:00"})
+
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
@@ -22,11 +30,11 @@
                   (ok result)))
 
            (POST "/ferry/next" []
-             :return {:depart java.time.LocalTime, :arrive java.time.LocalTime}
-             :query-params [{from :- String :oak} {to :- String :sffb}]
-             :summary      "Returns the next ferry between from and to. Defaults to Oakland Jack London to San Francisco Ferry Building"
-             (let [result (ferry/next-transit-time from to)]
-               (ok result)))))
+                 :return {:version String, :response {:outputSpeech {:type String :text String}}}
+                 :query-params [{from :- String :oak} {to :- String :sffb}]
+                 :summary      "Returns the next ferry between from and to. Defaults to Oakland Jack London to San Francisco Ferry Building"
+                 (let [result (ferry/next-transit-time from to)]
+                   (ok (to-alexa-response result))))))
 
 
 ;;(def from :oak)
