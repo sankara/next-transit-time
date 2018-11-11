e8d6bf0786a2a4bd40e1f42bd2aa7c68aa8d72e8
Sankara Rameswaran <hello@sankara.me>
2018-11-10 20:44:43 -0800

    Refactoring

--- src/clj/next_transit/bart.clj
+++ src/clj/next_transit/bart.clj
@@ -20,38 +20,23 @@
 
 ;;(def response (call-bart-api {:api "stn" :cmd "stns"} :client-params {:debug true}))
 
-(defn- fetch-stations []
-  (-> (call-bart-api {:api "stn" :cmd "stns"})
-      :root
-      :stations
-      :station))
-
 (def stations
-  (into {}
-        (map #(identity [(keyword (:abbr %)) %])
-             (fetch-stations))))
-;;stations
+  (memoize (fn []
+             (->> (call-bart-api {:api "stn" :cmd "stns"})
+                  :root
+                  :stations
+                  :station
+                  (map (fn [stn] [(keyword (:abbr stn)) stn]))
+                  (into {})))))
+;;(stations)
 
 (comment
   (defn- format-station-csv [[_ station]]
     (clojure.string/join "," (map #(str "\"" % "\"") (vals (select-keys station [:name, :abbr])))))
   (format-station-csv (first stations))
-  (map #(println (format-station-csv %)) stations))
+  (map #(println (format-station-csv %)) (stations)))
 
-
-(defn- parse-bart-time [time]
-  (try (t/local-time "h:mm a" time)
-       (catch java.time.format.DateTimeParseException e
-         (t/local-time "hh:mm a" time))))
-;;(parse-bart-time "11:59 PM")
-;;(parse-bart-time "1:10 AM")
-
-(defn- extract-time-values [keys names hash]
-  (let [parsed-values (map #(parse-bart-time (get hash (keyword %))) names)]
-    (apply assoc {} (interleave keys parsed-values))))
-;;(extract-time-values [:arrive] ["@origTimeMin"] (assoc {} (keyword "@origTimeMin") "11:50 AM"))
-
-(defn next-bart [from_k to_k]
+(defn- call-schedule-api [from_k to_k]
   (let [from (name from_k)
         to (name to_k)
         response (call-bart-api {:api "sched"
@@ -62,11 +47,40 @@
          :root
          :schedule
          :request
-         :trip
-         (map #(extract-time-values [from to]
-                                    ["@origTimeMin" "@destTimeMin"]
-                                    %))
-         (filter #(t/after? (get % from) (t/local-time))))))
+         :trip)))
+;;(call-schedule-api :24TH :ROCK)
+;;(def sched-hash (first (call-schedule-api :24TH :ROCK)))
+
+
+
+(defn- parse-bart-time [time]
+  (try (t/local-time "h:mm a" time)
+       (catch java.time.format.DateTimeParseException e
+         (t/local-time "hh:mm a" time))))
+;;(parse-bart-time "11:59 PM")
+;;(parse-bart-time "1:10 AM")
+
+(defn- extract-value [hash s]
+  (let [k (keyword (str "@" s))
+        v (get hash k)]
+    (if (re-find #"Time" s)
+      (parse-bart-time v)
+      (keyword v))))
+;;(extract-value sched-hash "origin")
+;;(extract-value sched-hash "origTimeMin")
+
+(defn- extract-schedule [sched-hash]
+  (apply assoc {}
+         (map (partial extract-value sched-hash)
+              ["origin" "origTimeMin" "destination" "destTimeMin"])))
+
+;;(extract-schedule sched-hash)
+
+
+(defn next-bart [from to]
+  (->> (call-schedule-api from to)
+       (map extract-schedule)
+       (filter #(t/after? (get % from) (t/local-time)))))
 
 ;; (next-bart :24TH :ROCK)
 ;; (next-bart :LAKE :EMBR)
